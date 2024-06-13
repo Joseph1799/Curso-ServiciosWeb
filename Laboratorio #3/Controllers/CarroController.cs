08d89a6d57ecb7a936ff91683ff9b18a5215726d
Joseph1799 <Joseph1799us@gmail.com>
2024-06-12 18:16:27 -0600

    Laboratorio #3: -Crear una API en C# con los métodos GET y POST

---
+++ Laboratorio #3/Controllers/CarroController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PruebaClienteAPI.Modelo;
+
+namespace PruebaClienteAPI.Controllers
+{
+    [Route("Carro")]
+    [ApiController]
+    public class CarroController : ControllerBase
+    {
+        [HttpGet]
+        [Route("listarCarros")]
+        public dynamic ListarCarros()
+        {
+            List<Carro> carrosList = new List<Carro>()
+            {
+                new Carro
+                {
+                    id="1",
+                    marca="Toyota",
+                    modelo="Camry",
+                    anio="2016",
+                    color="Gris",
+                },
+                new Carro
+                {
+                    id ="2",
+                    marca="Honda",
+                    modelo="Civic",
+                    anio="2018",
+                    color="Rojo",
+                },
+                new Carro
+                {
+                    id ="3",
+                    marca="Hyundai",
+                    modelo="Tucson",
+                    anio="2014",
+                    color="Blanco",
+                }
+            };
+            return carrosList;
+        }
+
+        [HttpPost]
+        [Route("guardarCarro")]
+        public dynamic guardarCarro(Carro carro)
+        {
+
+            carro.id = "4";
+            return new
+            {
+                success = true,
+                message = "Carro guardado",
+                Results = carro
+            };
+        }
+
+    }
+}
---
+++ Laboratorio #3/Modelo/Carro.cs
@@ -0,0 +1,12 @@
+﻿namespace PruebaClienteAPI.Modelo
+{
+    public class Carro
+    {
+        public string id { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public string anio { get; set;}
+        public string color { get; set;}    
+        
+    }
+}
